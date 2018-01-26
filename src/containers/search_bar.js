d589feda1e507b0cc14da98fc67cef595dfd1215
markhorvath <markhorvath7@gmail.com>
2018-01-26 17:20:35 -0500

    controlled components and binding context, onInputChange is a callback so had to bind it to this

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' };
+
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  onInputChange(event) {
+    this.setState({ term: event.target.value });
+    console.log(this.state.term);
+  }
+
   render() {
     return (
-      <form className="input-group">
-        <input />
+      <form className="input-group input-group-lg">
+        <input
+          placeholder="Get a 5 Day Forecast in Your Favorite Cities"
+          className="form-control"
+          value={this.state.term}
+          onChange={this.onInputChange}
+        />
         <span className="input-group-btn">
           <button type="submit" className="btn btn-secondary">
             Submit
