17e42bbd3611a5828b76e999c7f1476e14e89904
markhorvath <markhorvath7@gmail.com>
2018-01-27 14:47:37 -0500

    lec60, avoiding state mutations in reducers, changed reducer_weather to update state with payload

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,4 +1,18 @@
-export default function(state = null, action) {
-  console.log('ActioN received', action);
+import { FETCH_WEATHER } from '../actions/index';
+
+export default function(state = []], action) {
+  switch (action.type) {
+    case FETCH_WEATHER:
+      //This works for manipulating state (unlike push) and puts the new data at the end of the array
+      // return state.concat([action.payload.data]);
+
+
+      // Below is an ES6 format, says to create an array with the initial data (action.payload.data)
+      // and then the 3 dots lets it know it might be an array so take all entries from that array
+      // and add it to the first one (the one with action.payload.data)
+      // so it'd look like [city, city, city], NOT [city, [city, city]]
+      // but it would put the payload data as the first element in the array
+      return [ action.payload.data, ...state ];
+  }
   return state;
 }
