4d1e77faa65b623f06f6f0043ad63b1754788703
markhorvath <markhorvath7@gmail.com>
2018-01-26 23:53:46 -0500

    lec55-57, installed redux-promise and axios for api calls, added apikey, rooturl, and fetchWeather func

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+const API_KEY = 'e89738e89e334cbc1970b439c9906005';
+// The back ticks and ${API_KEY} are part of ES6, its the same as if
+// the url var was placed in quotes and then appended with + API_KEY;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
