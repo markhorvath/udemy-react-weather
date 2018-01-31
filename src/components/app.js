bfea37369a0c544a96a249e073f75338bc9a0d91
markhorvath <markhorvath7@gmail.com>
2018-01-31 15:59:42 -0500

    playing with styles

--- src/components/app.js
+++ src/components/app.js
@@ -8,6 +8,7 @@ export default class App extends Component {
     return (
       <div>
         <SearchBar />
+        <h1>Get Your 5 Day Weather Forecast for Any US City</h1>
         <WeatherList />
       </div>
     );
